8e6cbc8f2e1e4563e54906073441cdf24f0a415a
Gaurav Bhola <gaurav414u@gmail.com>
2018-02-09 12:44:17 +0530

    Not using AndroidInjection.inject in BaseActivity as it is taken care of in AppInjector class

--- app/src/main/java/com/bluestacks/bugzy/ui/BaseActivity.java
+++ app/src/main/java/com/bluestacks/bugzy/ui/BaseActivity.java
@@ -2,16 +2,10 @@ package com.bluestacks.bugzy.ui;
 
 
 import com.bluestacks.bugzy.data.local.PrefsHelper;
-
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-
 import javax.inject.Inject;
-
-import dagger.android.AndroidInjection;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
@@ -22,12 +16,6 @@ public class BaseActivity extends AppCompatActivity implements HasSupportFragmen
 
     @Inject
     DispatchingAndroidInjector<Fragment> fragmentInjector;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        AndroidInjection.inject(this);
-    }
 
     @Override
     public AndroidInjector<Fragment> supportFragmentInjector() {
