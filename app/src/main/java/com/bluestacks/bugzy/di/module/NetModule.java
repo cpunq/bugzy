ab4376e866febb535b12b78274304133d75aba98
Gaurav Bhola <gaurav414u@gmail.com>
2018-04-24 20:53:49 +0530

    Fixing a typo

--- app/src/main/java/com/bluestacks/bugzy/di/module/NetModule.java
+++ app/src/main/java/com/bluestacks/bugzy/di/module/NetModule.java
@@ -79,7 +79,7 @@ public class NetModule {
     FogbugzApiService provideFogBugzService(Application application, PrefsHelper prefsHelper, Gson gson, HostSelectionInterceptor hostSelectionInterceptor) {
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         httpClient.writeTimeout(2, TimeUnit.MINUTES);
-        httpClient.readTimeout(2, TimeUnit.DAYS.MINUTES);
+        httpClient.readTimeout(2, TimeUnit.MINUTES);
 
 
         Retrofit.Builder builder = new Retrofit.Builder()
